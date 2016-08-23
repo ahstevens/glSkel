ff3d63ae1df7e9b9b283565b7f921f68082bdeb1
ahstevens <ahstevens@gmail.com>
2016-08-22 22:00:07 -0400

    clean up mesh code

--- include/glSkel/mesh.h
+++ include/glSkel/mesh.h
@@ -15,51 +15,6 @@
 
 #include <glSkel/shader.h>
 
-struct HE_Vertex;
-struct HE_Face;
-struct HE_Edge;
-
-struct HE_Vertex {
-	glm::vec3 pos;
-	HE_Edge *halfedge;
-
-	HE_Vertex()
-		: pos(glm::vec3(0.f))
-		, halfedge(NULL)
-	{}
-};
-
-struct HE_Face {
-    HE_Edge *edge;
-	glm::vec3 normal;
-
-	HE_Face()
-		: edge(NULL)
-		, normal(glm::vec3(0.f))
-	{}
-};
-
-struct HE_Edge {
-	HE_Edge *next;
-	HE_Edge *opposite;
-	HE_Face *face;
-	HE_Vertex *head;
-
-	HE_Edge()
-		: next(NULL)
-		, opposite(NULL)
-		, face(NULL)
-		, head(NULL)
-	{}
-};
-
-
-struct Vertex {
-    glm::vec3 Position;
-    glm::vec3 Normal;
-    glm::vec2 TexCoords;
-};
-
 struct Texture {
     GLuint id;
     std::string type;
@@ -68,143 +23,51 @@ struct Texture {
 class Mesh {
 public:
     /*  Mesh Data  */
-	std::vector<Vertex> vertices;
 	std::vector<GLuint> indices;
-	std::vector<Texture> textures;
     GLuint VAO;
-
-	std::vector<HE_Edge*> edges;
-	std::vector<HE_Face*> faces;
-	std::vector<HE_Vertex*> verts;
-	HE_Edge *boundaryEdge;
 
 	glm::vec3 position;
 	GLfloat angle;
 
     /*  Functions  */
     // Constructor to make a DCEL mesh from a triangle soup
-    Mesh(std::vector<glm::vec3> vec3Vertices, std::vector<GLuint> indices, std::vector<Texture> textures)
+    Mesh(std::vector<glm::vec3> vvec3Vertices, std::vector<GLuint> vuiIndices, std::vector<Texture> vTextures)
     {
-        this->indices = indices;
-        this->textures = textures;
-		this->boundaryEdge = NULL;
+        this->indices = vuiIndices;
+        this->m_vTextures = vTextures;
+		this->m_pBoundaryEdge = NULL;
 
+		this->initializeVertices(vvec3Vertices);
 
-        for (int i = 0; i < vec3Vertices.size(); ++i)
-        {
-            HE_Vertex *v = new HE_Vertex();
-            v->pos = vec3Vertices[i];
-            verts.push_back(v);
-        }
+		this->processFacesAndEdges(vuiIndices);
 
-		typedef std::map< std::pair<int, int>, HE_Edge* > EdgeMapT;
-        EdgeMapT edgeMap;
+		std::cout << "Surface area: " << getSurfaceArea() << " cm^2 (" << m_vpFaces.size() << " faces)" << std::endl;
 
-        for (int i = 0; i < indices.size(); i += 3)
-        {
-            HE_Face *f = new HE_Face();
-
-			// Create the three halfedges of the face in CCW order
-            HE_Edge *e1, *e2, *e3;
-			e1 = new HE_Edge();
-			e2 = new HE_Edge();
-			e3 = new HE_Edge();
-
-            e1->face = f;
-            e1->next = e2;
-            e1->head = verts[indices[i + 1]];
-
-            e2->face = f;
-            e2->next = e3;
-            e2->head = verts[indices[i + 2]];
-
-            e3->face = f;
-            e3->next = e1;
-            e3->head = verts[indices[i]];
-
-            if (!verts[indices[i]]->halfedge) verts[indices[i]]->halfedge = e1;
-            if (!verts[indices[i+1]]->halfedge) verts[indices[i+1]]->halfedge = e2;
-            if (!verts[indices[i+2]]->halfedge) verts[indices[i+2]]->halfedge = e3;
-			
-			f->edge = e1;
-            faces.push_back(f);
-            edges.push_back(e1);
-            edges.push_back(e2);
-            edges.push_back(e3);
-
-            edgeMap[std::pair<int, int>(indices[i], indices[i + 1])] = e1;
-            edgeMap[std::pair<int, int>(indices[i + 1], indices[i + 2])] = e2;
-            edgeMap[std::pair<int, int>(indices[i + 2], indices[i])] = e3;
-        }
-        
-        for (EdgeMapT::iterator it = edgeMap.begin(); it != edgeMap.end(); it++)
-        {
-            if (it->second->opposite != NULL) continue;
-
-			EdgeMapT::iterator opp = edgeMap.find(std::pair<int, int>(it->first.second, it->first.first));
-            if(opp != edgeMap.end())
-            {
-                it->second->opposite = opp->second;
-                opp->second->opposite = it->second;
-            }
-			else // boundary edge
-			{
-				HE_Edge *newBoundaryEdge = new HE_Edge();
-				newBoundaryEdge->head = it->second->next->next->head;
-				newBoundaryEdge->face = NULL;
-				
-				// link new edge to next, if it exists
-				if (!newBoundaryEdge->head->halfedge->face)
-					newBoundaryEdge->next = newBoundaryEdge->head->halfedge;
-
-				// link opposite edges
-				newBoundaryEdge->opposite = it->second;
-				it->second->opposite = newBoundaryEdge;
-
-				// change emanating vertex to point to new boundary edge
-				it->second->head->halfedge = newBoundaryEdge;
-
-				edges.push_back(newBoundaryEdge);
-
-				if (!boundaryEdge) boundaryEdge = newBoundaryEdge;
-			}
-        }
-
-		for (int i = 0; i < edges.size(); ++i)
-			if (!edges[i]->next)
-				edges[i]->next = edges[i]->head->halfedge;
-
-		this->calculateFaceNormals();
-		
-		std::cout << "Surface area: " << getSurfaceArea() << " cm^2 (" << faces.size() << " faces)" << std::endl;
-		
 		std::cout << "Surface perimeter: " << getPerimeter() << " cm (" << getBoundaryEdgeCount() << " boundary edges)" << std::endl;
-
-		this->calculateVertexNormals();
 
         // Now that we have all the required data, set the vertex buffers and its attribute pointers.
         this->setupGL();
     }
 
-	unsigned int getVertexCount() { return verts.size(); }
+	unsigned int getVertexCount() { return m_vpVertices.size(); }
 
-	unsigned int getFaceCount()	{ return faces.size(); }
+	unsigned int getFaceCount()	{ return m_vpFaces.size(); }
 
-	unsigned int getHalfEdgeCount() { return edges.size(); }
+	unsigned int getHalfEdgeCount() { return m_vpEdges.size(); }
 
 	unsigned int getBoundaryEdgeCount()
 	{
-		if (!boundaryEdge)
+		if (!m_pBoundaryEdge)
 			return 0u;
 
-		HE_Edge *e = boundaryEdge;
+		HE_Edge *e = m_pBoundaryEdge;
 
 		unsigned int count = 0u;
 		do
 		{
 			count++;
 			e = e->next;
-		} while (e != boundaryEdge);
+		} while (e != m_pBoundaryEdge);
 
 		return count;
 	}
@@ -213,24 +76,24 @@ public:
 	{
 		float area = 0.f;
 
-		for (int i = 0; i < faces.size(); ++i)
-			area += glm::length(faces[i]->normal);     // face area is magnitude of face normal
+		for (int i = 0; i < m_vpFaces.size(); ++i)
+			area += glm::length(m_vpFaces[i]->normal);     // face area is magnitude of face normal
 
 		return area;
 	}
 
 	float getPerimeter()
 	{
-		if (!boundaryEdge)
+		if (!m_pBoundaryEdge)
 			return 0.f;
 
 		float p = 0.f;
-		HE_Edge *e = boundaryEdge;
+		HE_Edge *e = m_pBoundaryEdge;
 		do
 		{
 			p += glm::length(e->head->pos - e->opposite->head->pos);
 			e = e->next;
-		} while (e != boundaryEdge);
+		} while (e != m_pBoundaryEdge);
 
 		return p;
 	}
@@ -243,13 +106,13 @@ public:
 		GLuint specularNr = 1;
 		GLuint normalNr = 1;
 		GLuint heightNr = 1;
-		for (GLuint i = 0; i < this->textures.size(); i++)
+		for (GLuint i = 0; i < this->m_vTextures.size(); i++)
 		{
 			glActiveTexture(GL_TEXTURE0 + i); // Active proper texture unit before binding
 											  // Retrieve texture number (the N in diffuse_textureN)
 			std::stringstream ss;
 			std::string number;
-			std::string name = this->textures[i].type;
+			std::string name = this->m_vTextures[i].type;
 			if (name == "texture_diffuse")
 				ss << diffuseNr++;
 			else if (name == "texture_specular")
@@ -262,7 +125,7 @@ public:
 			// Now set the sampler to the correct texture unit
 			glUniform1i(glGetUniformLocation(shader.Program, (name + number).c_str()), i);
 			// And finally bind the texture
-			glBindTexture(GL_TEXTURE_2D, this->textures[i].id);
+			glBindTexture(GL_TEXTURE_2D, this->m_vTextures[i].id);
 		}
 
 		glm::mat4 model = glm::mat4();
@@ -277,7 +140,7 @@ public:
 		glBindVertexArray(0);
 
 		// Always good practice to set everything back to defaults once configured.
-		for (GLuint i = 0; i < this->textures.size(); i++)
+		for (GLuint i = 0; i < this->m_vTextures.size(); i++)
 		{
 			glActiveTexture(GL_TEXTURE0 + i);
 			glBindTexture(GL_TEXTURE_2D, 0);
@@ -285,78 +148,223 @@ public:
 	}
 
 private:
+
+	struct HE_Vertex;
+	struct HE_Face;
+	struct HE_Edge;
+
+	struct HE_Vertex {
+		glm::vec3 pos;
+		HE_Edge *halfedge;
+
+		HE_Vertex()
+			: pos(glm::vec3(0.f))
+			, halfedge(NULL)
+		{}
+	};
+
+	struct HE_Face {
+		HE_Edge *edge;
+		glm::vec3 normal;
+
+		HE_Face()
+			: edge(NULL)
+			, normal(glm::vec3(0.f))
+		{}
+	};
+
+	struct HE_Edge {
+		HE_Edge *next;
+		HE_Edge *opposite;
+		HE_Face *face;
+		HE_Vertex *head;
+
+		HE_Edge()
+			: next(NULL)
+			, opposite(NULL)
+			, face(NULL)
+			, head(NULL)
+		{}
+	};
+
+	struct Vertex {
+		glm::vec3 pos;
+		glm::vec3 norm;
+		glm::vec2 tex;
+	};
+
     /*  Render data  */
     GLuint VBO, EBO;
 
+	std::vector<HE_Edge*> m_vpEdges;
+	std::vector<HE_Face*> m_vpFaces;
+	std::vector<HE_Vertex*> m_vpVertices;
+	std::vector<Texture> m_vTextures;
+	HE_Edge *m_pBoundaryEdge;
+
     /*  Functions    */
-    // Initializes all the buffer objects/arrays
-    void setupGL()
-    {
-        // Create buffers/arrays
-        glGenVertexArrays(1, &this->VAO);
-        glGenBuffers(1, &this->VBO);
-        glGenBuffers(1, &this->EBO);
-
-        glBindVertexArray(this->VAO);
-        // Load data into vertex buffers
-        glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
-        // A great thing about structs is that their memory layout is sequential for all its items.
-        // The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
-        // again translates to 3/2 floats which translates to a byte array.
-        glBufferData(GL_ARRAY_BUFFER, this->vertices.size() * sizeof(Vertex), &this->vertices[0], GL_STATIC_DRAW);  
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->indices.size() * sizeof(GLuint), &this->indices[0], GL_STATIC_DRAW);
-
-        // Set the vertex attribute pointers
-        // Vertex Positions
-        glEnableVertexAttribArray(0);	
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)0);
-        // Vertex Normals
-        glEnableVertexAttribArray(1);	
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, Normal));
-        // Vertex Texture Coords
-        glEnableVertexAttribArray(2);	
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, TexCoords));
-
-        glBindVertexArray(0);
-    }
-
-	void calculateFaceNormals()
+	void initializeVertices(std::vector<glm::vec3> vertices)
 	{
-		for (int i = 0; i < faces.size(); ++i)
+		for (int i = 0; i < vertices.size(); ++i)
 		{
-			glm::vec3 vert1 = faces[i]->edge->head->pos;
-			glm::vec3 vert2 = faces[i]->edge->next->head->pos;
-			glm::vec3 vert3 = faces[i]->edge->next->next->head->pos;
-			glm::vec3 vecA = vert3 - vert2;
-			glm::vec3 vecB = vert1 - vert2;
-
-			faces[i]->normal = glm::cross(vecA, vecB); // calc face normal (not normalized)			
+			HE_Vertex *v = new HE_Vertex();
+			v->pos = vertices[i];
+			this->m_vpVertices.push_back(v);
 		}
 	}
 
-	void calculateVertexNormals()
+	void processFacesAndEdges(std::vector<GLuint> vuiIndices)
 	{
-		for (std::vector<HE_Vertex*>::iterator it = verts.begin(); it != verts.end(); it++)
+		// make an edge map to hold the index pairs of the vertices that make up
+		// the start and end of the half edge, respectively
+		typedef std::pair<int, int> EdgeMapIndexT;
+		typedef std::map< EdgeMapIndexT, HE_Edge* > EdgeMapT;
+		EdgeMapT edgeMap;
+
+		for (int i = 0; i < indices.size(); i += 3)
+		{
+			HE_Face *f = new HE_Face();
+
+			// Create the three halfedges of the face in CCW order
+			HE_Edge *e1, *e2, *e3;
+			e1 = new HE_Edge();
+			e2 = new HE_Edge();
+			e3 = new HE_Edge();
+
+			e1->face = f;
+			e1->next = e2;
+			e1->head = m_vpVertices[indices[i + 1]];
+
+			e2->face = f;
+			e2->next = e3;
+			e2->head = m_vpVertices[indices[i + 2]];
+
+			e3->face = f;
+			e3->next = e1;
+			e3->head = m_vpVertices[indices[i]];
+
+			if (!m_vpVertices[indices[i]]->halfedge) m_vpVertices[indices[i]]->halfedge = e1;
+			if (!m_vpVertices[indices[i + 1]]->halfedge) m_vpVertices[indices[i + 1]]->halfedge = e2;
+			if (!m_vpVertices[indices[i + 2]]->halfedge) m_vpVertices[indices[i + 2]]->halfedge = e3;
+
+			f->edge = e1; // doesn't matter which edge it points to
+			f->normal = glm::cross(e1->head->pos - e3->head->pos, e2->head->pos - e3->head->pos);
+			m_vpFaces.push_back(f);
+			m_vpEdges.push_back(e1);
+			m_vpEdges.push_back(e2);
+			m_vpEdges.push_back(e3);
+
+			// enter these 
+			edgeMap[EdgeMapIndexT(indices[i], indices[i + 1])] = e1;
+			edgeMap[EdgeMapIndexT(indices[i + 1], indices[i + 2])] = e2;
+			edgeMap[EdgeMapIndexT(indices[i + 2], indices[i])] = e3;
+		}
+
+		// link up paired half edges
+		for (EdgeMapT::iterator it = edgeMap.begin(); it != edgeMap.end(); it++)
+		{
+			if (it->second->opposite != NULL) continue;
+
+			// look for an edge going the opposite way in the edge map
+			EdgeMapT::iterator opp = edgeMap.find(std::pair<int, int>(it->first.second, it->first.first));
+			if (opp != edgeMap.end()) // found a match, so link 'em up
+			{
+				it->second->opposite = opp->second;
+				opp->second->opposite = it->second;
+			}
+			else // no match, therefore it's a boundary edge
+			{
+				HE_Edge *newBoundaryEdge = new HE_Edge();
+				newBoundaryEdge->head = it->second->next->next->head; // a boundary edge always points to boundary vertex 
+				newBoundaryEdge->face = NULL; // NULL face indicates a boundary edge
+
+				// link new edge to next, if it exists
+				if (!newBoundaryEdge->head->halfedge->face)
+					newBoundaryEdge->next = newBoundaryEdge->head->halfedge;
+
+				// link opposite edges
+				newBoundaryEdge->opposite = it->second;
+				it->second->opposite = newBoundaryEdge;
+
+				// change emanating vertex to point to new boundary edge.
+				// edges of boundary are oriented in CW order
+				it->second->head->halfedge = newBoundaryEdge;
+
+				m_vpEdges.push_back(newBoundaryEdge);
+				
+				// set the mesh boundary edge pointer the first time.
+				// NULL boundary edge pointer afterwards means it's a closed mesh
+				if (!m_pBoundaryEdge) m_pBoundaryEdge = newBoundaryEdge;
+			}
+		}
+
+		// one more pass to fill in any unlinked boundary edges
+		for (int i = 0; i < m_vpEdges.size(); ++i)
+			if (!m_vpEdges[i]->next)
+				m_vpEdges[i]->next = m_vpEdges[i]->head->halfedge;
+	}
+
+	std::vector<Vertex> makeBufferVertices()
+	{
+		std::vector<Vertex> ret;
+		for (std::vector<HE_Vertex*>::iterator it = m_vpVertices.begin(); it != m_vpVertices.end(); it++)
 		{
 			Vertex v;
-			v.Position = (*it)->pos;
-			v.Normal = glm::vec3(0.f);
-			v.TexCoords = glm::vec2(0.5f);
+			v.pos = (*it)->pos;
+			v.norm = glm::vec3(0.f);
+			v.tex = glm::vec2(0.5f);
 
+			// calc vertex normals as sum of (unnormalized) incident face normals
 			HE_Edge *beginEdge = (*it)->halfedge;
 			HE_Edge *e = beginEdge;
 			do
 			{
 				if (e->face)
-					v.Normal += e->face->normal;
+					v.norm += e->face->normal;
 				e = e->opposite->next;
 			} while (e != beginEdge);
-			v.Normal = glm::normalize(v.Normal);
 
-			this->vertices.push_back(v);
+			v.norm = glm::normalize(v.norm);
+
+			ret.push_back(v);
 		}
+
+		return ret;
+	}
+
+	// Initializes all the buffer objects/arrays
+	void setupGL()
+	{
+		std::vector<Vertex> bufferVertices = makeBufferVertices();
+
+		// Create buffers/arrays
+		glGenVertexArrays(1, &this->VAO);
+		glGenBuffers(1, &this->VBO);
+		glGenBuffers(1, &this->EBO);
+
+		glBindVertexArray(this->VAO);
+		// Load data into vertex buffers
+		glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+		// A great thing about structs is that their memory layout is sequential for all its items.
+		// The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
+		// again translates to 3/2 floats which translates to a byte array.
+		glBufferData(GL_ARRAY_BUFFER, bufferVertices.size() * sizeof(Vertex), &bufferVertices[0], GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->indices.size() * sizeof(GLuint), &this->indices[0], GL_STATIC_DRAW);
+
+		// Set the vertex attribute pointers
+		// Vertex Positions
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)0);
+		// Vertex Normals
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, norm));
+		// Vertex Texture Coords
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, tex));
+
+		glBindVertexArray(0);
 	}
 };
 
