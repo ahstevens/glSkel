326879979c97005f60755e07234cef04dd12e150
Drew Stevens <ahstevens@gmail.com>
2016-11-23 14:28:03 -0500

    change quat to matrix
    quats r hard :(

--- include/glSkel/Object.h
+++ include/glSkel/Object.h
@@ -8,12 +8,12 @@ class Object
 public:
 	Object()
 		: m_vec3Position(glm::vec3())
-		, m_quatOrientation(glm::quat())
+		, m_mat4Rotation(glm::mat4())
 	{}
 
-	Object(glm::vec3 position, glm::quat orientation)
+	Object(glm::vec3 position, glm::mat4 rot)
 		: m_vec3Position(position)
-		, m_quatOrientation(orientation)
+		, m_mat4Rotation(rot)
 	{}
 
 	~Object() {}
@@ -21,8 +21,8 @@ public:
 	void setPosition(glm::vec3 pos) { m_vec3Position = pos; }
 	glm::vec3 getPosition() { return m_vec3Position; }
 
-	void setOrientation(glm::quat o) { m_quatOrientation = o; }
-	glm::quat getOrientation() { return m_quatOrientation; }
+	void setOrientation(glm::mat4 rot) { m_mat4Rotation = rot; }
+	glm::mat4 getOrientation() { return m_mat4Rotation; }
 
 	void setScale(glm::vec3 s) { m_vec3Scale = s; }
 	void setScale(float s) { m_vec3Scale = glm::vec3(s); }
@@ -30,7 +30,7 @@ public:
 
 protected:
 	glm::vec3 m_vec3Position;
-	glm::quat m_quatOrientation;
+	glm::mat4 m_mat4Rotation;
 	glm::vec3 m_vec3Scale;
 };
 
